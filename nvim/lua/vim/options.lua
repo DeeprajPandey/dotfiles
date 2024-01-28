15ea529b335d26bb3ccbdc26f67d08b408297f08
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2024-01-28 23:02:28 +0530

    Turn off auto lib provider lookups

--- nvim/lua/vim/options.lua
+++ nvim/lua/vim/options.lua
@@ -3,3 +3,9 @@ vim.cmd "set tabstop=2"
 vim.cmd "set softtabstop=2"
 vim.cmd "set shiftwidth=2"
 
+-- turn off automatic lib provider lookup
+vim.cmd "let g:loaded_python3_provider = 0"
+vim.cmd "let g:loaded_ruby_provider = 0"
+vim.cmd "let g:loaded_node_provider = 0"
+vim.cmd "let g:loaded_perl_provider = 0"
+
