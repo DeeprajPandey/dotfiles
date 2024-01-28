e20f7cb0ea530266b1aea26eaa7c27527275cf2d
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2024-01-28 23:54:45 +0530

    Add more options and remaps

--- nvim/lua/vim/options.lua
+++ nvim/lua/vim/options.lua
@@ -1,7 +1,30 @@
-vim.cmd "set expandtab"
-vim.cmd "set tabstop=2"
-vim.cmd "set softtabstop=2"
-vim.cmd "set shiftwidth=2"
+-- line numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- indent options
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undo-history"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
 
 -- turn off automatic lib provider lookup
 vim.cmd "let g:loaded_python3_provider = 0"
--- nvim/lua/vim/remap.lua
+++ nvim/lua/vim/remap.lua
@@ -1,2 +1,11 @@
 vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Shift+j,k moves selected text down, up
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Ctrl+d,u moves text down, up w/ centered cursor
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
