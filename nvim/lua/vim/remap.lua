dc7f09fd232c29f801f86180a11e390c57321750
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2024-02-17 17:24:25 +0530

    Add all vim keymaps courtesy of primeagen

--- nvim/lua/vim/remap.lua
+++ nvim/lua/vim/remap.lua
@@ -1,16 +1,49 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
--- Shift+j,k moves selected text down, up
+-- shift+j,k moves selected text down, up
 -- known issue: J,K updates undotree
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- Ctrl+d,u moves text down, up w/ centered cursor
+-- keep cursor at current location when appending lines
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- ctrl+d,u scrolls half-page down, up w/ centered cursor
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
--- Bring search results to the middle of the screen
-vim.keymap.set("n", "n", "nzz")
-vim.keymap.set("n", "N", "Nzz")
+-- keep search results to the middle of the screen
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- alt paste where delete during paste is mapped to void register to preserve yanked text
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- alt yank mapped to + register (sys clipboard)
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- alt delete to void register in normal and visual modes
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- paste in vertical visual mode works only if Esc is used to exit mode
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- substitute current word
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- make current file executable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- press leader twice to source current file
+vim.keymap.set("n", "<leader><leader>", function()
+vim.cmd("so")
+end)
 
