4c44af1ded8e142a56de36d906459d4b3d532fac
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2024-02-17 11:22:20 +0530

    Add comments to vim options and update column width to 110

--- nvim/lua/vim/options.lua
+++ nvim/lua/vim/options.lua
@@ -11,20 +11,24 @@ vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
 vim.opt.wrap = false
 
+-- no vim undo actions; give undotree access to full hist file
+vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undo-history"
 vim.opt.undofile = true
 
+-- incremental search highlight but not the search results
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+-- min. 8 lines of scroll offset
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "110"
 
 -- turn off automatic lib provider lookup
 vim.cmd "let g:loaded_python3_provider = 0"
