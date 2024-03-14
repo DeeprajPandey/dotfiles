a00a3fd3f548812085f07e07fa9cb389a166299c
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2024-03-14 10:29:26 +0530

    Install nnn, btop; uninstall broot, exa (deprecated); noinstall fusion360

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -9,16 +9,16 @@ alias l='ls'
 # Use colors in coreutils utilities output
 alias grep='grep --color'
 
-# ls aliases - make sure exa is installed
-alias ls='exa --group-directories-first --icons -aG'
-alias le='exa --icons -aG'
-alias ld='exa --icons --time-style=default -s=Filename -FlaghHD'
-alias la='exa --group-directories-first --colour-scale --icons --time-style=default -s=Filename -FlaghHS'
-alias LD='exa --icons --time-style=default -s=Filename -laghHiD'
-alias LA='exa --group-directories-first --colour-scale --icons --time-style=default -s=Filename -laghHiS'
-# Tree commands which need a level number, e.g. `lt 2`
-alias lt='exa --tree --icons --group-directories-first -L'
-alias ltd='exa --tree --icons -aDL'
+# # ls aliases - make sure exa is installed
+# alias ls='exa --group-directories-first --icons -aG'
+# alias le='exa --icons -aG'
+# alias ld='exa --icons --time-style=default -s=Filename -FlaghHD'
+# alias la='exa --group-directories-first --colour-scale --icons --time-style=default -s=Filename -FlaghHS'
+# alias LD='exa --icons --time-style=default -s=Filename -laghHiD'
+# alias LA='exa --group-directories-first --colour-scale --icons --time-style=default -s=Filename -laghHiS'
+# # Tree commands which need a level number, e.g. `lt 2`
+# alias lt='exa --tree --icons --group-directories-first -L'
+# alias ltd='exa --tree --icons -aDL'
 
 # Aliases to protect against overwriting
 alias cp='cp -i'
