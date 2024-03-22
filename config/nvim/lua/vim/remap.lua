f8a197b0bab185ebf53e7989e958ed0645d69abe
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2024-03-22 19:04:13 +0530

    Update mapping for <leader>pv to open Lexplore instead of Ex

--- config/nvim/lua/vim/remap.lua
+++ config/nvim/lua/vim/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", "<cmd>Lexplore<CR>", {desc = "Open project drawer"})
 
 -- shift+j,k moves selected text down, up
 -- known issue: J,K updates undotree
