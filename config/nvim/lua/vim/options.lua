d9f45e27d761304bcd80a4ba36981dbde2c551bf
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2024-03-22 11:28:05 +0530

    Add editor markers and vim config from vimrc

--- config/nvim/lua/vim/options.lua
+++ config/nvim/lua/vim/options.lua
@@ -1,38 +1,67 @@
+local opt = vim.opt
+local cmd = vim.cmd
+
 -- line numbers
-vim.opt.nu = true
-vim.opt.relativenumber = true
+opt.nu = true
+opt.rnu = true
 
 -- indent options
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-
-vim.opt.smartindent = true
-vim.opt.wrap = false
+opt.autoindent = false
+opt.expandtab = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.smartindent = true
+opt.wrap = false
 
 -- no vim undo actions; give undotree access to full hist file
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undo-history"
-vim.opt.undofile = true
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undo-history"
+opt.undofile = true
 
--- incremental search highlight but not the search results
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+-- incremental search highlight
+opt.hlsearch =  true
+opt.incsearch = true
 
--- min. 8 lines of scroll offset (+3 for treesitter-context & separator)
-vim.opt.scrolloff = 11
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+-- min. 8 lines of scroll offopt (+3 for treesitter-context & separator)
+opt.scrolloff = 11
+opt.signcolumn = "yes"
+opt.isfname:append("@-@")
 
-vim.opt.updatetime = 50
+opt.updatetime = 50
 
-vim.opt.colorcolumn = "110"
+-- vertical lines to mark indent levels
+-- extends:▶,precedes:◀
+opt.listchars = "leadmultispace:▏   ,lead:￮,tab:» ,trail:·,extends:›,precedes:‹,nbsp:‿"
+opt.list = true
+opt.lbr = true
+
+-- mark column to limit text to 110 chars
+opt.colorcolumn = "110"
+opt.lazyredraw = true
+
+-- allow auto-hiding of edited buffers
+opt.hidden = true
+
+-- smart case-sensitive search
+opt.ignorecase = true
+opt.smartcase = true
+
+-- mouse mode
+opt.mouse = "a"
+
+-- disable audible bell
+opt.errorbells = false
+opt.visualbell = true
+
+-- open new split panes to the right and bottom
+opt.splitbelow = true
+opt.splitright = true
 
 -- turn off automatic lib provider lookup
-vim.cmd "let g:loaded_python3_provider = 0"
-vim.cmd "let g:loaded_ruby_provider = 0"
-vim.cmd "let g:loaded_node_provider = 0"
-vim.cmd "let g:loaded_perl_provider = 0"
+cmd "let g:loaded_python3_provider = 0"
+cmd "let g:loaded_ruby_provider = 0"
+cmd "let g:loaded_node_provider = 0"
+cmd "let g:loaded_perl_provider = 0"
 
