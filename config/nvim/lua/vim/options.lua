cfb063ced17cb4ad0bd6963d97236af1c77cdc40
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2024-03-23 01:41:09 +0530

    Add configs and remaps from sensible.vim (tpope)
    Most are set by default nvim

--- config/nvim/lua/vim/options.lua
+++ config/nvim/lua/vim/options.lua
@@ -1,3 +1,16 @@
+-- Pointers ref. sensible.vim: most config values are added to nvim by default
+-- ttimeoutlen is set to 50ms
+-- smarttab: true
+-- `complete` does not include 'i'; no need to attempt removing it
+-- laststatus: 2
+-- ruler: true
+-- wildmenu: true
+-- display: 'lastline'
+-- formatoptions: 'tcqj'
+-- autoread: true
+-- tabpagemax: 50
+-- sessionoptions and viewoptions do not have options by default
+-- langremap: false
 local opt = vim.opt
 local cmd = vim.cmd
 
@@ -14,15 +27,28 @@ opt.softtabstop = 4
 opt.smartindent = true
 opt.wrap = false
 
+-- vertical lines to mark indent levels
+-- extends:▶,precedes:◀
+opt.listchars = "leadmultispace:▏   ,lead:￮,tab:» ,trail:·,extends:›,precedes:‹,nbsp:‿"
+opt.list = true
+opt.lbr = true
+
+-- horizontal scrolling and offset columns
+opt.sidescroll = 1
+opt.sidescrolloff = 3   -- 1 col is extends and precedes symbols
+
 -- no vim undo actions; give undotree access to full hist file
 opt.swapfile = false
 opt.backup = false
+opt.writebackup = false
 opt.undodir = os.getenv("HOME") .. "/.vim/undo-history"
 opt.undofile = true
 
 -- incremental search highlight
-opt.hlsearch =  true
 opt.incsearch = true
+
+-- highlight all search matches
+opt.hlsearch =  true
 
 -- min. 8 lines of scroll offopt (+3 for treesitter-context & separator)
 opt.scrolloff = 11
@@ -30,12 +56,6 @@ opt.signcolumn = "yes"
 opt.isfname:append("@-@")
 
 opt.updatetime = 50
-
--- vertical lines to mark indent levels
--- extends:▶,precedes:◀
-opt.listchars = "leadmultispace:▏   ,lead:￮,tab:» ,trail:·,extends:›,precedes:‹,nbsp:‿"
-opt.list = true
-opt.lbr = true
 
 -- mark column to limit text to 110 chars
 opt.colorcolumn = "110"
