3209e4f2cc8c2cd084bf0e79517691e59b6af0e3
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2024-03-31 15:22:24 +0530

    Add cellular automaton and vim startup time

---
+++ config/nvim/lua/prsnl/plugins/cellular-auotomaton.lua
@@ -0,0 +1,9 @@
+M = {
+  'eandrju/cellular-automaton.nvim',
+  keys = {
+    { '<leader>mr', '<cmd>CellularAutomaton make_it_rain<cr>', desc = '[M]ake it [R]ain', noremap = true, silent = true },
+  },
+}
+
+return M
+
