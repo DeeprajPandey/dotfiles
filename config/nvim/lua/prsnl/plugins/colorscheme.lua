dfd862ab657cdd6c7d2809401ec066f495029192
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2024-04-09 16:42:15 +0530

    Reformat and add nightfly transparency toggle function

--- config/nvim/lua/prsnl/plugins/colorscheme.lua
+++ config/nvim/lua/prsnl/plugins/colorscheme.lua
@@ -16,140 +16,142 @@ local M = {
     }
   },
   -- 2
-  { 'bluz71/vim-nightfly-colors', name = 'nightfly', },
+  { "rose-pine/neovim", name = "rose-pine" },
   -- 3
-  { 'rebelot/kanagawa.nvim', name = 'kanagawa', },
+  { 'bluz71/vim-nightfly-colors', name = 'nightfly', },
   -- 4
-  { 'EdenEast/nightfox.nvim', name = 'nightfox', },
+  { 'rebelot/kanagawa.nvim',      name = 'kanagawa', },
   -- 5
-  { 'savq/melange-nvim', name = 'melange', },
+  { 'EdenEast/nightfox.nvim',     name = 'nightfox', },
   -- 6
---  -- source: https://github.com/thanhvule0310/dotfiles/blob/main/nvim/lua/v/plugins/catppuccin.lua
---  {
---    'catppuccin/nvim',
---    name = 'catppuccin',
---    lazy = false,
---    priority = 1000,
---    opts = {
---      background = {
---        light = 'latte',
---        dark = 'mocha',
---      },
---      transparent_background = false,
---      show_end_of_buffer = false,
---      integration_default = false,
---      integrations = {
---        aerial = true,
---        alpha = true,
---        barbecue = { dim_dirname = true, bold_basename = true, dim_context = false, alt_background = false },
---        cmp = true,
---        dashboard = true,
---        flash = true,
---        gitsigns = true,
---        headlines = true,
---        illuminate = true,
---        indent_blankline = { enabled = true },
---        leap = true,
---        lsp_trouble = true,
---        markdown = true,
---        mason = true,
---        mini = true,
---        native_lsp = {
---          enabled = true,
---          inlay_hints = { background = true },
---          underlines = {
---            errors = { 'undercurl' },
---            hints = { 'undercurl' },
---            warnings = { 'undercurl' },
---            information = { 'undercurl' },
---          },
---        },
---        navic = { enabled = true, custom_bg = 'lualine' },
---        neogit = true,
---        neotest = true,
---        neotree = true,
---        noice = true,
---        notify = true,
---        semantic_tokens = true,
---        telescope = true,
---        treesitter = true,
---        treesitter_context = true,
---        vimwiki = true,
---        which_key = true,
---      },
---      highlight_overrides = {
---        all = function(colors)
---          return {
---            NormalFloat = { bg = colors.base },
---            Pmenu = { bg = colors.mantle, fg = '' },
---            PmenuSel = { bg = colors.surface0, fg = '' },
---            CursorLineNr = { fg = colors.text },
---            LineNr = { fg = colors.surface1 },
---            FloatBorder = { bg = colors.base, fg = colors.surface0 },
---            VertSplit = { bg = colors.base, fg = colors.surface0 },
---            WinSeparator = { bg = colors.base, fg = colors.surface0 },
---            LspInfoBorder = { link = 'FloatBorder' },
---            YankHighlight = { bg = colors.surface2 },
---
---            CmpItemMenu = { fg = colors.surface2 },
---
---            GitSignsChange = { fg = colors.peach },
---
---            NeoTreeDirectoryIcon = { fg = colors.subtext1 },
---            NeoTreeDirectoryName = { fg = colors.subtext1 },
---            NeoTreeFloatBorder = { link = 'TelescopeResultsBorder' },
---            NeoTreeGitConflict = { fg = colors.red },
---            NeoTreeGitDeleted = { fg = colors.red },
---            NeoTreeGitIgnored = { fg = colors.overlay0 },
---            NeoTreeGitModified = { fg = colors.peach },
---            NeoTreeGitStaged = { fg = colors.green },
---            NeoTreeGitUnstaged = { fg = colors.red },
---            NeoTreeGitUntracked = { fg = colors.green },
---            NeoTreeIndent = { link = 'IblIndent' },
---            NeoTreeNormal = { bg = colors.mantle },
---            NeoTreeNormalNC = { bg = colors.mantle },
---            NeoTreeRootName = { fg = colors.subtext1, style = { 'bold' } },
---            NeoTreeTabActive = { fg = colors.text, bg = colors.mantle },
---            NeoTreeTabInactive = { fg = colors.surface2, bg = colors.crust },
---            NeoTreeTabSeparatorActive = { fg = colors.mantle, bg = colors.mantle },
---            NeoTreeTabSeparatorInactive = { fg = colors.crust, bg = colors.crust },
---            NeoTreeWinSeparator = { fg = colors.base, bg = colors.base },
---
---            TelescopePreviewBorder = { bg = colors.crust, fg = colors.crust },
---            TelescopePreviewNormal = { bg = colors.crust },
---            TelescopePreviewTitle = { fg = colors.crust, bg = colors.crust },
---            TelescopePromptBorder = { bg = colors.surface0, fg = colors.surface0 },
---            TelescopePromptCounter = { fg = colors.mauve, style = { 'bold' } },
---            TelescopePromptNormal = { bg = colors.surface0 },
---            TelescopePromptPrefix = { bg = colors.surface0 },
---            TelescopePromptTitle = { fg = colors.surface0, bg = colors.surface0 },
---            TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
---            TelescopeResultsNormal = { bg = colors.mantle },
---            TelescopeResultsTitle = { fg = colors.mantle, bg = colors.mantle },
---            TelescopeSelection = { bg = colors.surface0 },
---
---            WhichKeyFloat = { bg = colors.mantle },
---
---            IblIndent = { fg = colors.surface0 },
---            IblScope = { fg = colors.overlay0 },
---
---            MasonNormal = { bg = colors.mantle },
---            MasonMutedBlock = { link = 'CursorLine' },
---
---            LazyNormal = { bg = colors.mantle },
---          }
---        end,
---        latte = function(colors)
---          return {
---            LineNr = { fg = colors.surface1 },
---
---            IblIndent = { fg = colors.mantle },
---            IblScope = { fg = colors.surface1 },
---          }
---        end,
---      },
---    },
---  },
+  { 'savq/melange-nvim',          name = 'melange', },
+  -- 7
+  --  -- source: https://github.com/thanhvule0310/dotfiles/blob/main/nvim/lua/v/plugins/catppuccin.lua
+  --  {
+  --    'catppuccin/nvim',
+  --    name = 'catppuccin',
+  --    lazy = false,
+  --    priority = 1000,
+  --    opts = {
+  --      background = {
+  --        light = 'latte',
+  --        dark = 'mocha',
+  --      },
+  --      transparent_background = false,
+  --      show_end_of_buffer = false,
+  --      integration_default = false,
+  --      integrations = {
+  --        aerial = true,
+  --        alpha = true,
+  --        barbecue = { dim_dirname = true, bold_basename = true, dim_context = false, alt_background = false },
+  --        cmp = true,
+  --        dashboard = true,
+  --        flash = true,
+  --        gitsigns = true,
+  --        headlines = true,
+  --        illuminate = true,
+  --        indent_blankline = { enabled = true },
+  --        leap = true,
+  --        lsp_trouble = true,
+  --        markdown = true,
+  --        mason = true,
+  --        mini = true,
+  --        native_lsp = {
+  --          enabled = true,
+  --          inlay_hints = { background = true },
+  --          underlines = {
+  --            errors = { 'undercurl' },
+  --            hints = { 'undercurl' },
+  --            warnings = { 'undercurl' },
+  --            information = { 'undercurl' },
+  --          },
+  --        },
+  --        navic = { enabled = true, custom_bg = 'lualine' },
+  --        neogit = true,
+  --        neotest = true,
+  --        neotree = true,
+  --        noice = true,
+  --        notify = true,
+  --        semantic_tokens = true,
+  --        telescope = true,
+  --        treesitter = true,
+  --        treesitter_context = true,
+  --        vimwiki = true,
+  --        which_key = true,
+  --      },
+  --      highlight_overrides = {
+  --        all = function(colors)
+  --          return {
+  --            NormalFloat = { bg = colors.base },
+  --            Pmenu = { bg = colors.mantle, fg = '' },
+  --            PmenuSel = { bg = colors.surface0, fg = '' },
+  --            CursorLineNr = { fg = colors.text },
+  --            LineNr = { fg = colors.surface1 },
+  --            FloatBorder = { bg = colors.base, fg = colors.surface0 },
+  --            VertSplit = { bg = colors.base, fg = colors.surface0 },
+  --            WinSeparator = { bg = colors.base, fg = colors.surface0 },
+  --            LspInfoBorder = { link = 'FloatBorder' },
+  --            YankHighlight = { bg = colors.surface2 },
+  --
+  --            CmpItemMenu = { fg = colors.surface2 },
+  --
+  --            GitSignsChange = { fg = colors.peach },
+  --
+  --            NeoTreeDirectoryIcon = { fg = colors.subtext1 },
+  --            NeoTreeDirectoryName = { fg = colors.subtext1 },
+  --            NeoTreeFloatBorder = { link = 'TelescopeResultsBorder' },
+  --            NeoTreeGitConflict = { fg = colors.red },
+  --            NeoTreeGitDeleted = { fg = colors.red },
+  --            NeoTreeGitIgnored = { fg = colors.overlay0 },
+  --            NeoTreeGitModified = { fg = colors.peach },
+  --            NeoTreeGitStaged = { fg = colors.green },
+  --            NeoTreeGitUnstaged = { fg = colors.red },
+  --            NeoTreeGitUntracked = { fg = colors.green },
+  --            NeoTreeIndent = { link = 'IblIndent' },
+  --            NeoTreeNormal = { bg = colors.mantle },
+  --            NeoTreeNormalNC = { bg = colors.mantle },
+  --            NeoTreeRootName = { fg = colors.subtext1, style = { 'bold' } },
+  --            NeoTreeTabActive = { fg = colors.text, bg = colors.mantle },
+  --            NeoTreeTabInactive = { fg = colors.surface2, bg = colors.crust },
+  --            NeoTreeTabSeparatorActive = { fg = colors.mantle, bg = colors.mantle },
+  --            NeoTreeTabSeparatorInactive = { fg = colors.crust, bg = colors.crust },
+  --            NeoTreeWinSeparator = { fg = colors.base, bg = colors.base },
+  --
+  --            TelescopePreviewBorder = { bg = colors.crust, fg = colors.crust },
+  --            TelescopePreviewNormal = { bg = colors.crust },
+  --            TelescopePreviewTitle = { fg = colors.crust, bg = colors.crust },
+  --            TelescopePromptBorder = { bg = colors.surface0, fg = colors.surface0 },
+  --            TelescopePromptCounter = { fg = colors.mauve, style = { 'bold' } },
+  --            TelescopePromptNormal = { bg = colors.surface0 },
+  --            TelescopePromptPrefix = { bg = colors.surface0 },
+  --            TelescopePromptTitle = { fg = colors.surface0, bg = colors.surface0 },
+  --            TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
+  --            TelescopeResultsNormal = { bg = colors.mantle },
+  --            TelescopeResultsTitle = { fg = colors.mantle, bg = colors.mantle },
+  --            TelescopeSelection = { bg = colors.surface0 },
+  --
+  --            WhichKeyFloat = { bg = colors.mantle },
+  --
+  --            IblIndent = { fg = colors.surface0 },
+  --            IblScope = { fg = colors.overlay0 },
+  --
+  --            MasonNormal = { bg = colors.mantle },
+  --            MasonMutedBlock = { link = 'CursorLine' },
+  --
+  --            LazyNormal = { bg = colors.mantle },
+  --          }
+  --        end,
+  --        latte = function(colors)
+  --          return {
+  --            LineNr = { fg = colors.surface1 },
+  --
+  --            IblIndent = { fg = colors.mantle },
+  --            IblScope = { fg = colors.surface1 },
+  --          }
+  --        end,
+  --      },
+  --    },
+  --  },
   -- source: https://github.com/catppuccin/nvim/discussions/323?sort=top#discussioncomment-5287724
   {
     'catppuccin/nvim',
@@ -519,9 +521,14 @@ local function toggleCatppuccinTransparency()
   vim.cmd.colorscheme 'catppuccin'
 end
 
+local function toggleNightflyTransparency()
+  local isTransparent = vim.g.nightflyTransparent
+  vim.g.nightflyTransparent = not isTransparent
+end
+
 M[2].lazy = false
 M[2].priority = true
-M[2].config= function(_, opts)
+M[2].config = function(_, opts)
   -- substitution: `.,+1s/tokyonight/catppuccin/g`
   -- require('catppuccin').setup(opts)
   -- vim.cmd.colorscheme 'catppuccin'
@@ -533,10 +540,9 @@ M[2].config= function(_, opts)
 
   -- vim.cmd.hi 'Comment gui=none'
 
-  vim.keymap.set('n', '<leader>0', toggleCatppuccinTransparency, {
+  vim.keymap.set('n', '<leader>0', toggleNightflyTransparency, {
     desc = 'Toggle Catppuccin transparency', noremap = true, silent = true,
   })
 end
 
 return M
-
