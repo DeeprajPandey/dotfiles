b6e62133837e2fbeeeb1306b27c8744cb2ce283b
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2024-04-10 18:21:34 +0530

    Refactor and setup autoloading sessions on VimEnter

--- config/nvim/lua/prsnl/plugins/vim-obsession.lua
+++ config/nvim/lua/prsnl/plugins/vim-obsession.lua
@@ -19,13 +19,19 @@ function M.config(_, opts)
   local function load_session(session_file)
     if vim.fn.filereadable(session_file) == 1 then
       vim.cmd('source ' .. session_file)
+    else
+      print('Session `' .. session_file .. '` not found.')
     end
+  end
+
+  local function create_session(session_file)
+    vim.cmd('Obsession ' .. session_file)
   end
 
   local function session_save()
     local session_file = vim.fn.input('Session file: ', get_default_session_file(), 'file')
     if session_file and #session_file > 0 then
-      vim.cmd('Obsession ' .. session_file)
+      create_session(session_file)
     end
   end
 
@@ -34,14 +40,32 @@ function M.config(_, opts)
     load_session(session_file)
   end
 
+  local function session_autoload()
+    local session_file = get_default_session_file()
+    if vim.fn.filereadable(session_file) == 1 then
+      load_session(session_file)
+    else
+      create_session(session_file)
+    end
+  end
+
   local nmap = function(keys, func, desc)
     vim.keymap.set('n', keys, func, { desc = desc, noremap = true, silent = true })
   end
 
+  -- keymaps
   nmap('<leader>os', session_save, '[O]bsession [S]ave')
   nmap('<leader>or', session_restore, '[O]bsession [R]estore')
   nmap('<leader>op', '<cmd>Obsession<CR>', '[O]bsession [P]ause')
   nmap('<leader>ox', '<cmd>Obsession!<CR>', '[O]bsession Close and Delete[x]')
+
+  -- autocommands
+  vim.api.nvim_create_autocmd('VimEnter', {
+    desc = 'Autoload session saved by vim-obsession',
+    group = vim.api.nvim_create_augroup('vim_obsession-autoload', { clear = true }),
+    pattern = '*',
+    callback = session_autoload
+  })
 end
 
 return M
