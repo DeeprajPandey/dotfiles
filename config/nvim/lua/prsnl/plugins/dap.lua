d24c94f315319baed6ffd7d165f91da76428be7a
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2024-04-17 16:10:06 +0530

    Add dap-ui and configure dap to work with ui

--- config/nvim/lua/prsnl/plugins/dap.lua
+++ config/nvim/lua/prsnl/plugins/dap.lua
@@ -14,9 +14,24 @@ M = {
       desc = '[D]AP: Toggle [b]reakpoint'
     },
   },
+  dependencies = {
+    {
+      'rcarriga/nvim-dap-ui',
+      dependencies = { 'nvim-neotest/nvim-nio' },
+    },
+  },
 }
 
 function M.config(_, opts)
+  local dap = require 'dap'
+  local ui_ok, dapui = pcall(require, 'dapui')
+
+  if not ui_ok then return end
+  -- dap.listeners.before.attach['dapui_config'] = dapui.open
+  -- dap.listeners.before.launch['dapui_config'] = dapui.open
+  dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+  dap.listeners.after.event_terminated['dapui_config'] = dapui.close
+  dap.listeners.after.event_exited['dapui_config'] = dapui.close
 end
 
 return M
