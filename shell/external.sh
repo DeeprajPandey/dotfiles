c645124de5a2469290f6b99679a9e6eed1913084
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2024-04-23 11:02:52 +0530

    Force reinstall node and nvm to play well with tsserver

--- shell/external.sh
+++ shell/external.sh
@@ -14,8 +14,10 @@ export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
 export DOCKER_SCAN_SUGGEST=false
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+
 
 # Create pipenv virtualenvs in project directory to avoid pipenv install on project path restructures
 # https://pipenv.pypa.io/en/latest/installation.html#virtualenv-mapping-caveat
