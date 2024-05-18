8131ebda02afb58bd1832ba3d8c8f5a7ab33603f
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2024-05-18 09:40:40 +0530

    Add autocmd group for highlights and add autocmd to clear references and highlight autocmds when detaching from LSP

--- config/nvim/lua/prsnl/plugins/lsp/lspconfig.lua
+++ config/nvim/lua/prsnl/plugins/lsp/lspconfig.lua
@@ -51,14 +51,25 @@ function M.config(_, opts)
       -- ref: `:help CursorHold`
       local client = vim.lsp.get_client_by_id(event.data.client_id)
       if client and client.server_capabilities.documentHighlightProvider then
+        local highlight_augroup = vim.api.nvim_create_augroup('prsnl-lsp-highlight', { clear = false })
         vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
           buffer = event.buf,
+          group = highlight_augroup,
           callback = vim.lsp.buf.document_highlight,
         })
 
         vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
           buffer = event.buf,
+          group = highlight_augroup,
           callback = vim.lsp.buf.clear_references,
+        })
+
+        vim.api.nvim_create_autocmd('LspDetach', {
+          group = vim.api.nvim_create_augroup('prsnl-lsp-detach', { clear = true }),
+          callback = function(event2)
+            vim.lsp.buf.clear_references()
+            vim.api.nvim_clear_autocmds { group = 'prsnl-lsp-highlight', buffer = event2.buf }
+          end,
         })
       end
     end,
@@ -119,7 +130,7 @@ function M.config(_, opts)
     bashls = {},
     biome = {},
     clangd = {
-    capabilities = {
+      capabilities = {
         offsetEncoding = 'utf-16',
       },
     },
