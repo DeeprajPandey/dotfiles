96a19bb76d6497030f5840a69e2ad1f15f80fd94
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2024-06-11 09:04:37 +0530

    Add cargo binary to path. rustup post-installation instructions

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -9,6 +9,10 @@ path_append "$HOME/.luarocks/bin"
 # symlinks for compilers which will use ccache automatically
 path_prepend "/opt/homebrew/opt/ccache/libexec"
 
+# for rustup: need cargo's bin dir in path (prepended based on comments in `~/.cargo/env`)
+# prepend: in case a system-installed rustc needs to be overridden
+path_prepend "$HOME/.cargo/bin"
+
 # Reduces delay when entering vi-mode
 export KEYTIMEOUT=1
 
