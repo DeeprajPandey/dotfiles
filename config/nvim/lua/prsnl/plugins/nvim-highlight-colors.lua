d9849d106e020a796c39959e18dd027f7a41c802
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2024-10-03 00:41:11 +0530

    Add nvim-highlight-colors

---
+++ config/nvim/lua/prsnl/plugins/nvim-highlight-colors.lua
@@ -0,0 +1,11 @@
+M = {
+  'brenoprata10/nvim-highlight-colors',
+  lazy = true,
+  opt = {
+    -- options: 'background'|'foreground'|'virtual'
+    render = 'virtual',
+    enable_tailwind = true,
+  },
+}
+
+return M
