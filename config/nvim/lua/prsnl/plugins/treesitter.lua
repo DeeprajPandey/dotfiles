ed3f61300611c51af7703f1d8a57b2af57da0cc2
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2024-10-07 19:04:02 +0530

    Add config for incremental selection, context, and textobjects

--- config/nvim/lua/prsnl/plugins/treesitter.lua
+++ config/nvim/lua/prsnl/plugins/treesitter.lua
@@ -1,13 +1,12 @@
--- TODO: look into treesitter-textobjects, treesitter-context, and incremental selection
 local M = {
   'nvim-treesitter/nvim-treesitter',
   event = 'VeryLazy',
   build = ':TSUpdate',
   dependencies = {
-    'nvim-treesitter/playground'
+    'nvim-treesitter/playground',
   },
   opts = {
-    ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'query', 'rust', 'vim', 'vimdoc', },
+    ensure_installed = { 'bash', 'c', 'go', 'html', 'lua', 'luadoc', 'markdown', 'query', 'rust', 'vim', 'vimdoc' },
 
     sync_install = false,
     auto_install = true,
@@ -16,8 +15,76 @@ local M = {
       enable = true,
       additional_vim_regex_highlighting = false,
     },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<space>', -- maps in normal mode to init the node/scope selection with space
+        node_incremental = '<space>', -- increment to the upper named parent
+        node_decremental = '<bs>', -- decrement to the previous node
+        scope_incremental = '<tab>', -- increment to the upper scope (as defined in locals.scm)
+      },
+    },
     -- experimental
     indent = { enable = true },
+    autopairs = {
+      enable = true,
+    },
+    context = {
+      enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+      max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+      min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+      line_numbers = true,
+      multiline_threshold = 20, -- Maximum number of lines to show for a single context
+      trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+      mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
+      -- Separator between context and content. Should be a single character string, like '-'.
+      -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+      separator = nil,
+      zindex = 20, -- The Z-index of the context window
+      on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ['aa'] = '@parameter.outer',
+          ['ia'] = '@parameter.inner',
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+          ['iB'] = '@block.inner',
+          ['aB'] = '@block.outer',
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          [']]'] = '@function.outer',
+        },
+        goto_next_end = {
+          [']['] = '@function.outer',
+        },
+        goto_previous_start = {
+          ['[['] = '@function.outer',
+        },
+        goto_previous_end = {
+          ['[]'] = '@function.outer',
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          ['<leader>sn'] = '@parameter.inner',
+        },
+        swap_previous = {
+          ['<leader>sp'] = '@parameter.inner',
+        },
+      },
+    },
   },
 }
 
@@ -41,4 +108,3 @@ function M.config(_, opts)
 end
 
 return M
-
