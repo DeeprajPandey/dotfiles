ca8fcbe3e39344e4f58356b5f6a97a2ad38ba358
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2025-05-04 17:09:21 +0530

    refactor: undotree setup function

--- config/nvim/lua/prsnl/plugins/undotree.lua
+++ config/nvim/lua/prsnl/plugins/undotree.lua
@@ -1,8 +1,10 @@
 M = {
   'mbbill/undotree',
-  config = function()
-    vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
-  end
+  event = 'VimEnter',
 }
+
+function M.config(_, opts)
+  vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+end
 
 return M
